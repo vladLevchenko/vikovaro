b8dd52f5a6e5913e47308ba11b45e766c81916ca
VLevchenko <Volodymyr.Levchenko@onseo.biz>
2015-12-17 18:14:02 +0200

    added tab view for groups

---
+++ VKTestApp/VKTestApp.Droid/TabbedCarouselActivity.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V4.App;
+
+
+namespace VKTestApp.Droid
+{
+	[Activity (Label = "TabbedCarouselActivity")]			
+	public class TabbedCarouselActivity : FragmentActivity
+	{
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+			SetContentView (Resource.Layout.TabbedCarouselView);
+
+			// Create your application here
+		}
+	}
+}
+
